28054dfd702cd2c2c68c7290a69de112ee71c2c8
Daniel Oliveira <67245632+danrmoliv@users.noreply.github.com>
2025-10-11 16:42:00 -0300

    Create models Listing, Comment and Bid. Edit index.html

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,35 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64, blank=False)
+    description = models.CharField(max_length=5000, blank=False)
+    min_price = models.FloatField(blank=False)
+    image_url = models.CharField(max_length=500, blank=True)
+    category = models.CharField(max_length=64, blank=True)
+    listed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f"{self.title} - Price: {self.min_price} - Listed by: {self.listed_by}"
+    
+class Bid(models.Model):
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids_on_product")
+    bid_value = models.FloatField(blank=False)
+    user_bid = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids_by_user')
+
+    def __str__(self):
+        return f"{self.product} - Bid: {self.bid_value} - Bid from: {self.user_bid}"
+
+class Comment(models.Model):
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    comment = models.CharField(max_length=5000, blank=False)
+    user_comment = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments_by_user')
+
+    def __str__(self):
+        return f"{self.product} - {self.comment}"
